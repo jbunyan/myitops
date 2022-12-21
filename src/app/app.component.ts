20f1a82e9dda8e5258f90015911d88ebff86a574
ISS Builder <iss@macbuilder01.int-link.com>
2022-12-21 15:17:56 +0000

    adding BBD icon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { SplashScreen } from '@capacitor/splash-screen';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,7 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor() {
+    SplashScreen.hide();
+  }
 }
